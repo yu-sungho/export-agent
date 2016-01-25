3035788dc11fa757c8c3ae326f70904b558eaa7f
yu-sungho <sunghoyu.yeah@gmail.com>
2016-01-26 02:19:56 +0900

    spring boot example

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,14 +1,24 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @RestController
 public class HelloController {
+
+    static final Logger log = LoggerFactory.getLogger(HelloController.class);
     
     @RequestMapping("/")
     public String index() {
+        log.info("#########################################", "test");
         return "Greetings from Spring Boot!";
+    }
+    @RequestMapping("/file/export")
+    public void export() {
+        //Document doc = Jsoup.get
     }
     
 }
